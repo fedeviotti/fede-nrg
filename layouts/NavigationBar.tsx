7b874baf34bebacad5a502bf2cba33e1845e318f
Federico Viotti <fedeviotti@gmail.com>
2022-07-23 15:54:49 +0200

    refactor: move color mode toggle on navbar

--- layouts/NavigationBar.tsx
+++ layouts/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, ButtonGroup, Flex, Heading} from "@chakra-ui/react";
+import {Button, ButtonGroup, Flex, Heading, useColorMode} from "@chakra-ui/react";
 import NextLink from "next/link";
 
 type Props = {
@@ -8,6 +8,8 @@ type Props = {
 }
 
 const NavigationBar = ({showHomeButton, children}: Props) => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <Flex direction="column" alignItems="center" width="100%" height="100vh">
       <Flex backgroundColor="green.100" width="100%" px="24px" py="16px" justifyContent="space-between">
@@ -24,6 +26,9 @@ const NavigationBar = ({showHomeButton, children}: Props) => {
           <NextLink href='#' passHref>
             <Button as="a">Memory</Button>
           </NextLink>
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+          </Button>
         </ButtonGroup>
       </Flex>
       <Flex backgroundColor="green.300" width="60%" height="100%">
