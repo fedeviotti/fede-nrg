741ab80970a17014d146f6b605e5d804a03338c7
Federico Viotti <fedeviotti@gmail.com>
2022-07-24 11:18:49 +0200

    feat: adding form floating label

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,6 +1,16 @@
 import { useState } from 'react';
 import { supabase } from '../lib/SupabaseClient';
-import { Button, Flex, Heading, Input, Text} from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  Text
+} from "@chakra-ui/react";
 
 export default function Auth() {
   const [loading, setLoading] = useState(false)
@@ -23,12 +33,24 @@ export default function Auth() {
     <Flex direction="column" gap="8px">
       <Heading>Supabase + Next.js</Heading>
       <Text>Sign in via magic link with your email below</Text>
-      <Input
-        type="email"
-        placeholder="Your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+
+      <FormControl
+        id="email"
+        variant="floating"
+        isRequired
+        isInvalid
+      >
+        <Input
+          type="email"
+          placeholder=" "
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {/* It is important that the Label comes after the Control due to css selectors */}
+        <FormLabel>Email</FormLabel>
+        <FormHelperText>Keep it very short and sweet!</FormHelperText>
+        <FormErrorMessage>Your First name is invalid</FormErrorMessage>
+      </FormControl>
       <Button
         onClick={(e) => {
           e.preventDefault()
--- themes/theme.tsx
+++ themes/theme.tsx
@@ -2,14 +2,50 @@ import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const fonts = {
   heading: `'Poppins', sans-serif`,
-  body: `'Poppins', sans serif`
+  body: `'Poppins', sans serif`,
+
 };
+
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px)"
+};
+
+const components = {
+  Form: {
+    variants: {
+      floating: {
+        container: {
+          _focusWithin: {
+            label: {
+              ...activeLabelStyles
+            }
+          },
+          "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label": {
+            ...activeLabelStyles
+          },
+          label: {
+            top: 0,
+            left: 0,
+            zIndex: 2,
+            position: "absolute",
+            backgroundColor: "white",
+            pointerEvents: "none",
+            mx: 3,
+            px: 1,
+            my: 2,
+            transformOrigin: "left top"
+          }
+        }
+      }
+    }
+  }
+}
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config, fonts });
+const theme = extendTheme({ config, fonts, components });
 
 export default theme
