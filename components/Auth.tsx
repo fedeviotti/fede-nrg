98edfc21156c067c393dd4e340b127bf34b6b391
Federico Viotti <fedeviotti@gmail.com>
2022-07-24 12:35:22 +0200

    feat: add auth validation form

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,25 +1,29 @@
 import { useState } from 'react';
 import { supabase } from '../lib/SupabaseClient';
+import {Field, FieldInputProps, FieldMetaProps, Form, Formik} from "formik";
+import * as yup from "yup";
 import {
   Button,
   Flex,
   FormControl,
   FormErrorMessage,
-  FormHelperText,
   FormLabel,
   Heading,
   Input,
   Text
 } from "@chakra-ui/react";
 
+type AuthFormValues = {
+  email: string;
+}
+
 export default function Auth() {
   const [loading, setLoading] = useState(false)
-  const [email, setEmail] = useState('')
 
-  const handleLogin = async (email: string) => {
+  const handleSubmit = async (submittedValues: AuthFormValues) => {
     try {
       setLoading(true)
-      const { error } = await supabase.auth.signIn({ email })
+      const { error } = await supabase.auth.signIn({ email: submittedValues.email })
       if (error) throw error
       alert('Check your email for the login link!')
     } catch (error: any) {
@@ -29,37 +33,57 @@ export default function Auth() {
     }
   }
 
+  const loginSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email()
+      .required(),
+  });
+
   return (
     <Flex direction="column" gap="8px">
       <Heading>Supabase + Next.js</Heading>
       <Text>Sign in via magic link with your email below</Text>
 
-      <FormControl
-        id="email"
-        variant="floating"
-        isRequired
-        isInvalid
+      <Formik
+        initialValues={{email: ""}}
+        onSubmit={handleSubmit}
+        validationSchema={loginSchema}
       >
-        <Input
-          type="email"
-          placeholder=" "
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {/* It is important that the Label comes after the Control due to css selectors */}
-        <FormLabel>Email</FormLabel>
-        <FormHelperText>Keep it very short and sweet!</FormHelperText>
-        <FormErrorMessage>Your First name is invalid</FormErrorMessage>
-      </FormControl>
-      <Button
-        onClick={(e) => {
-          e.preventDefault()
-          handleLogin(email)
-        }}
-        disabled={loading}
-      >
-        {loading ? 'Loading' : 'Send magic link'}
-      </Button>
+        {({isSubmitting, isValid, dirty} ) => (
+        <Form>
+          <Flex direction="column" gap="8px">
+            <Field name='email'>
+              {({ field, meta }: {field: FieldInputProps<any>, meta: FieldMetaProps<any>}) => (
+                <FormControl
+                  id="email"
+                  variant="floating"
+                  isInvalid={meta.touched && !!meta.error}
+                >
+                  {/*<pre>{JSON.stringify(meta, null, 2)}</pre>*/}
+                  <Input
+                    {...field}
+                    placeholder=" "
+                    value={field.value}
+                    onChange={(e) => field.onChange(e)}
+                  />
+                  <FormLabel>Email</FormLabel>
+                  {/*<FormHelperText>Keep it very short and sweet!</FormHelperText>*/}
+                  <FormErrorMessage>{meta.error}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button
+              alignSelf="flex-start"
+              disabled={!(isValid && dirty)}
+              isLoading={isSubmitting}
+              type='submit'
+            >
+              {loading ? 'Loading' : 'Send magic link'}
+            </Button>
+          </Flex>
+        </Form>)}
+      </Formik>
     </Flex>
   )
 }
