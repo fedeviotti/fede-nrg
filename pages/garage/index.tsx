4810ed9406f5861ad9a6f56dad13c350b81b8a79
Federico Viotti <fedeviotti@gmail.com>
2022-08-02 22:28:48 +0200

    chore: fix based on linting (2)

--- pages/garage/index.tsx
+++ pages/garage/index.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-import {Box} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Head from "next/head";
 import NavigationBar from "../../layouts/NavigationBar";
 
-const Garage = () => {
-  return (
-    <>
-      <Head>
-        <title>Garage manager</title>
-        <meta name="description" content="Garage manager application" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NavigationBar shouldShowHomeButton shouldShowFeatures>
-        <Box>Garage Page</Box>
-      </NavigationBar>
-    </>
-  )
-}
+const Garage = () => (
+  <>
+    <Head>
+      <title>Garage manager</title>
+      <meta name="description" content="Garage manager application" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <NavigationBar shouldShowHomeButton shouldShowFeatures>
+      <Box>Garage Page</Box>
+    </NavigationBar>
+  </>
+);
 
 export default Garage;
