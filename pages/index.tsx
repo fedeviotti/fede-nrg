8e15a9e659f3a8226ce3c0cfcf1c0518d7841e65
Federico Viotti <fedeviotti@gmail.com>
2022-08-02 09:13:19 +0200

    refactor: first linted file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import {Box, Flex} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Box, Flex } from "@chakra-ui/react";
 import { Session } from "@supabase/supabase-js";
 import NavigationBar from "../layouts/NavigationBar";
-import { supabase } from '../lib/SupabaseClient';
-import Auth from '../components/Auth'
-import Account from '../components/Account'
+import { supabase } from "../lib/SupabaseClient";
+import Auth from "../components/Auth";
+import Account from "../components/Account";
 
 const Home: NextPage = () => {
   const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
-    setSession(supabase.auth.session())
+    setSession(supabase.auth.session());
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
+    supabase.auth.onAuthStateChange((_event, _session) => {
+      setSession(_session);
+    });
   }, []);
 
   return (
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
         </Flex>
       </NavigationBar>
     </>
-  )
-}
+  );
+};
 
 export default Home;
