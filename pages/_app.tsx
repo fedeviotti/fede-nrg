5d18d52ebfc219d9ea2817e2e900ce23a7caaf87
Federico Viotti <fedeviotti@gmail.com>
2022-08-02 22:28:13 +0200

    chore: fix based on linting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,32 @@
-import '../styles/globals.css'
-import '@fontsource/poppins/100.css'
-import '@fontsource/poppins/200.css'
-import '@fontsource/poppins/300.css'
-import '@fontsource/poppins/400.css'
-import '@fontsource/poppins/500.css'
-import '@fontsource/poppins/600.css'
-import '@fontsource/poppins/700.css'
-import '@fontsource/poppins/800.css'
-import '@fontsource/poppins/900.css'
-import '@fontsource/poppins/100-italic.css'
-import '@fontsource/poppins/200-italic.css'
-import '@fontsource/poppins/300-italic.css'
-import '@fontsource/poppins/400-italic.css'
-import '@fontsource/poppins/500-italic.css'
-import '@fontsource/poppins/600-italic.css'
-import '@fontsource/poppins/700-italic.css'
-import '@fontsource/poppins/800-italic.css'
-import '@fontsource/poppins/900-italic.css'
+import "../styles/globals.css";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
+import "@fontsource/poppins/900.css";
+import "@fontsource/poppins/100-italic.css";
+import "@fontsource/poppins/200-italic.css";
+import "@fontsource/poppins/300-italic.css";
+import "@fontsource/poppins/400-italic.css";
+import "@fontsource/poppins/500-italic.css";
+import "@fontsource/poppins/600-italic.css";
+import "@fontsource/poppins/700-italic.css";
+import "@fontsource/poppins/800-italic.css";
+import "@fontsource/poppins/900-italic.css";
 
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../themes/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
-export default MyApp
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,14 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, {
+  Html, Head, Main, NextScript,
+} from "next/document";
 import theme from "../themes/theme";
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
@@ -13,6 +16,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
