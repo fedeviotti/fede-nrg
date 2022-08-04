44ee9e1182f898a303821ce3e1491a952a11232f
Federico Viotti <fedeviotti@gmail.com>
2022-08-05 00:10:05 +0200

    feat: get all bikes

--- pages/garage/index.tsx
+++ pages/garage/index.tsx
@@ -6,7 +6,9 @@ import NavigationBar from "../../layouts/NavigationBar";
 import { supabase } from "../../lib/SupabaseClient";
 import { definitions } from "../../types/supabase";
 
-const fetcher = async () => supabase.from<definitions["vehicles"]>("vehicles").select("*").single();
+const fetcher = async () => supabase
+  .from<definitions["vehicles"]>("vehicles")
+  .select("*");
 
 const Garage = () => {
   const { data, error } = useSWR("/api/vehicles", fetcher);
@@ -24,9 +26,10 @@ const Garage = () => {
       <NavigationBar shouldShowHomeButton shouldShowFeatures>
         <Box display="flex" flexDirection="column" gap="16px">
           <Box>Garage Page</Box>
-          {/* <pre>{JSON.stringify(data.body, null, 2)}</pre> */}
-          <Box>{data.body?.name}</Box>
-          <Box>{data.data?.name}</Box>
+          {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+          {data.data?.map((vehicle) => (
+            <Box key={vehicle.id}>{vehicle.name}</Box>
+          ))}
         </Box>
       </NavigationBar>
     </>
