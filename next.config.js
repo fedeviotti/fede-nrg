ac4ae239979667c579a962e41f11f7cc823c0f70
Federico Viotti <fedeviotti@gmail.com>
2022-08-07 22:10:43 +0200

    chore: babel and next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    forceSwcTransforms: true
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
