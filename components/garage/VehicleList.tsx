4fa4427f8b9e9decf6a1b9b015a8881faf2e831d
Federico Viotti <fedeviotti@gmail.com>
2022-08-09 19:12:34 +0200

    chore: create VehicleList component

--- components/garage/VehicleList.tsx
+++ components/garage/VehicleList.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { supabase } from "~/lib/SupabaseClient";
 import useSWR, { Fetcher, Key } from "swr";
 import { Box, Stack } from "@chakra-ui/react";
-import { Vehicles as Vehicle } from "@prisma/client";
 import { VehicleCard } from "~/components/garage/VehicleCard";
+import { ExtendedVehicle } from "~/types/garage/vehicle";
 
-const fetcher: Fetcher<Vehicle[], string> = (url: string) => fetch(url).then((res) => res.json());
+const fetcher:
+Fetcher<ExtendedVehicle[], string> = (url: string) => fetch(url).then((res) => res.json());
 
 export const VehicleList = () => {
   const user = supabase.auth.user();
@@ -16,7 +17,7 @@ export const VehicleList = () => {
   if (!vehicles) return <Box>Loading ...</Box>;
 
   return (
-    <Stack direction="column">
+    <Stack direction="row">
       {/* <pre>{JSON.stringify(vehicles, null, 2)}</pre> */}
       {vehicles?.map((vehicle) => (
         <VehicleCard key={Number(vehicle.id)} vehicle={vehicle} />
