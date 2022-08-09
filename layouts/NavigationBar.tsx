d138f92a95e988f9a321c49c3fabe6ccd4f82853
Federico Viotti <fedeviotti@gmail.com>
2022-08-09 23:35:53 +0200

    refactor: layout change on navbar

--- layouts/NavigationBar.tsx
+++ layouts/NavigationBar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import {
-  Button, ButtonGroup, Flex, Heading, useColorMode,
+  Button, ButtonGroup, Flex, Image, useColorMode, useColorModeValue,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
+import LOGO_LIGHT from "~/assets/FEDENRG_LOGO_LIGHT.png";
+import LOGO_DARK from "~/assets/FEDENRG_LOGO_DARK.png";
 
 type Props = {
   shouldShowHomeButton?: boolean;
@@ -12,6 +14,7 @@ type Props = {
 
 const NavigationBar = ({ shouldShowHomeButton, shouldShowFeatures, children }: Props) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const logoSrc = useColorModeValue(LOGO_LIGHT.src, LOGO_DARK.src);
 
   return (
     <Flex direction="column" alignItems="center" width="100vw" height="100vh">
@@ -22,7 +25,13 @@ const NavigationBar = ({ shouldShowHomeButton, shouldShowFeatures, children }: P
         justifyContent="space-between"
         boxShadow={colorMode === "light" ? "rgba(0, 0, 0, .05) 0px 1px 2px" : "rgba(255, 255, 255, .1) 0px 1px 2px"}
       >
-        <Heading as="h1" fontWeight="thin" fontSize="xx-large" letterSpacing="wide">Fede Nrg Site</Heading>
+        <Image
+          alt="FedeNrg Logo"
+          rounded="lg"
+          height={10}
+          objectFit="cover"
+          src={logoSrc}
+        />
         <ButtonGroup>
           {shouldShowHomeButton && (
             <NextLink href="/" passHref>
@@ -39,14 +48,14 @@ const NavigationBar = ({ shouldShowHomeButton, shouldShowFeatures, children }: P
               </NextLink>
             </>
           )}
-          <Button onClick={toggleColorMode}>
+          <Button onClick={toggleColorMode} variant="outline">
             Toggle
             {" "}
             {colorMode === "light" ? "Dark" : "Light"}
           </Button>
         </ButtonGroup>
       </Flex>
-      <Flex width="100%" justifyContent="center" pt="32px">{children}</Flex>
+      <Flex justifyContent="center" p={8} width="full" height="full">{children}</Flex>
     </Flex>
   );
 };
