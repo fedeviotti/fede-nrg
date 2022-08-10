7973b82407fdf2b0db979394c717dc89969db1a0
Federico Viotti <fedeviotti@gmail.com>
2022-08-10 22:35:00 +0200

    refactor: update memory and garage page

--- pages/garage/index.tsx
+++ pages/garage/index.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Box, Heading } from "@chakra-ui/react";
 import Head from "next/head";
-import NavigationBar from "~/layouts/NavigationBar";
 import { VehicleList } from "~/components/garage/VehicleList";
+import { NavbarLayout } from "~/layouts/NavbarLayout";
 
 const Garage = () => (
   <>
     <Head>
-      <title>Garage manager</title>
-      <meta name="description" content="Garage manager application" />
+      <title>Garage Manager</title>
+      <meta name="description" content="Garage Manager" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <NavigationBar shouldShowHomeButton shouldShowFeatures>
+    <NavbarLayout>
       <Box display="flex" flexDirection="column" gap="16px" alignItems="center">
-        <Heading as="h2" size="xl" fontWeight="semibold">Garage Page</Heading>
+        <Heading as="h2" size="xl" fontWeight="semibold">Garage Manager</Heading>
         <VehicleList />
       </Box>
-    </NavigationBar>
+    </NavbarLayout>
   </>
 );
 
