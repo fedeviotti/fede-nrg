6357effcc68ebdb539040d5d671c978cd65e9934
Federico Viotti <fedeviotti@gmail.com>
2022-08-14 11:35:21 +0200

    chore: add setSession and setUser on AuthProvider

--- lib/AuthProvider.tsx
+++ lib/AuthProvider.tsx
@@ -22,6 +22,18 @@ export const AuthProvider = ({ supabase, ...props }: Props) => {
   const [session, setSession] = React.useState<Session | null>(null);
   const [user, setUser] = React.useState<User | null>(null);
 
+  React.useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, currentSession) => {
+        setSession(currentSession);
+        setUser(currentSession?.user ?? null);
+      },
+    );
+    return () => {
+      authListener?.unsubscribe();
+    };
+  }, [supabase.auth]);
+
   const contextValue = React.useMemo(() => ({
     session,
     user,
