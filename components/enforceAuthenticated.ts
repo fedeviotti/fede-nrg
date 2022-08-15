5feaa960d073bb703a856fc6536ef50959632e1f
Federico Viotti <fedeviotti@gmail.com>
2022-08-15 07:14:46 +0200

    fix: destination url on root

--- components/enforceAuthenticated.ts
+++ components/enforceAuthenticated.ts
@@ -6,9 +6,11 @@ type EnforceAuthenticated = (inner?: GetServerSideProps) => GetServerSideProps;
 const enforceAuthenticated: EnforceAuthenticated = (inner) => async (context) => {
   const { req, resolvedUrl } = context;
   const { user } = await supabase.auth.api.getUserByCookie(req);
+  const baseDestination = "/login";
+  const redirect = resolvedUrl !== "/" ? `?redirect=${resolvedUrl}` : "";
 
   if (!user) {
-    return { props: {}, redirect: { destination: `/login?redirect=${resolvedUrl}` } };
+    return { props: {}, redirect: { destination: `${baseDestination}${redirect}` } };
   }
 
   if (inner) {
