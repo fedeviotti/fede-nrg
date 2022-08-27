fefbcea281e35b2aac31a23f48b047fc8101243b
Federico Viotti <fedeviotti@gmail.com>
2022-08-27 23:36:45 +0200

    chore(i18n): nextjs config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-var path = require("path");
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
     locales: ["en", "it"],
     defaultLocale: "it",
-    localePath: path.resolve("./locales")
+    localeDetection: false
   }
 }
 
