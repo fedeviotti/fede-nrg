1a7a6cbe26779eebe6a13b44cae47a1f9acc3520
Federico Viotti <fedeviotti@gmail.com>
2022-08-27 15:11:36 +0200

    chore(i18n): fix nextjs warning

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n
+  i18n: {
+    locales: i18n.locales,
+    defaultLocale: i18n.defaultLocale
+  }
 }
 
 module.exports = nextConfig
