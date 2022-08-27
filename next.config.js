5e129ca123bf3f1aa3bf306cd8a8dbd1d6e6efee
Federico Viotti <fedeviotti@gmail.com>
2022-08-27 22:35:21 +0200

    chore(i18n): react-i18next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  i18n: {
+    locales: ['en', 'it'],
+    defaultLocale: 'it'
+  }
 }
 
 module.exports = nextConfig
