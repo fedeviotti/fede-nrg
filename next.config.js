5c4643692aff760907f736fb13dd7e00da16b47d
Federico Viotti <fedeviotti@gmail.com>
2022-08-27 23:20:45 +0200

    chore(i18n): fix for vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+var path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ['en', 'it'],
-    defaultLocale: 'it'
+    locales: ["en", "it"],
+    defaultLocale: "it",
+    localePath: path.resolve("./locales")
   }
 }
 
