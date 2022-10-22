98ec0110d59ae31af1e94ec8a6c4b99b9266f19f
Federico Viotti <fedeviotti@gmail.com>
2022-10-22 23:33:06 +0200

    feat(garage): update form ui and save vehicle

--- components/garage/VehicleForm.tsx
+++ components/garage/VehicleForm.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {
-  Box, Button, Flex, Heading,
+  Box, Button, Flex, Heading, useToast, Text,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import * as yup from "yup";
 import { Field, Form, Formik } from "formik";
+import { supabase } from "~/lib/initSupabaseClient";
+import { defaultToastOptions } from "~/lib/constants/defaultToastOptions";
+import { useAuth } from "~/lib/context/AuthProvider";
 
 type VehicleFormValues = {
   name: string;
@@ -27,14 +30,45 @@ const initialValues = {
 
 export const VehicleForm = () => {
   const { t } = useTranslation("common");
+  const toast = useToast();
+  const { user } = useAuth();
 
-  const handleSubmit = React.useCallback((values: VehicleFormValues) => {
-    console.log("values", values);
-  }, []);
+  const handleSubmit = React.useCallback(async (values: VehicleFormValues) => {
+    const { data, error } = await supabase
+      .from("vehicles")
+      .insert([
+        {
+          name: values.name,
+          description: values.description,
+          created_at: new Date(),
+          updated_at: new Date(),
+          is_owned: true,
+          type_id: 7,
+          owner_id: user?.id,
+        },
+      ]);
+    if (data) {
+      toast({
+        title: t("garage.vehicle.create_form.toast.title"),
+        description: t("garage.vehicle.create_form.toast.success"),
+        status: "success",
+        ...defaultToastOptions,
+      });
+    }
+    if (error) {
+      toast({
+        title: t("garage.vehicle.create_form.toast.title"),
+        description: error.message || t("garage.vehicle.create_form.toast.error"),
+        status: "error",
+        ...defaultToastOptions,
+      });
+    }
+  }, [t, toast, user?.id]);
 
   return (
     <Box borderWidth="1px" borderRadius="lg" p="6">
-      <Heading>{t("garage.vehicle.form.title")}</Heading>
+      <Heading>{t("garage.vehicle.create_form.title")}</Heading>
+      <Text fontSize="md">{t("garage.vehicle.create_form.description")}</Text>
       <Formik
         initialValues={initialValues}
         onSubmit={handleSubmit}
@@ -43,14 +77,21 @@ export const VehicleForm = () => {
         {({ isSubmitting, isValid }) => (
           <Form id="create-vehicle">
             <Flex direction="column" gap="8px">
-              <Field name="name" />
-              <Field name="description" />
+              <Field
+                name="name"
+                placeholder={t("garage.vehicle.create_form.field.name")}
+              />
+              <Field
+                name="description"
+                placeholder={t("garage.vehicle.create_form.field.description")}
+              />
               <Button
+                type="submit"
                 form="create-vehicle"
                 isLoading={isSubmitting}
                 isDisabled={!isValid}
               >
-                {t("garage.vehicle.form.cta")}
+                {t("garage.vehicle.create_form.cta")}
               </Button>
             </Flex>
           </Form>
