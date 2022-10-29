6473577c4d76cfa1a99c07c7b87d0e75f6eb9deb
Federico Viotti <fedeviotti@gmail.com>
2022-10-29 23:00:26 +0200

    feat(footer): add footer translations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
 
-export const Footer = () => (
-  <Flex minH={16} justifyContent="center" alignItems="center">
-    Made with ❤️ and ☕️ by FEDENRG
-  </Flex>
-);
+export const Footer = () => {
+  const { t } = useTranslation("common");
+  return (
+    <Flex minH={16} justifyContent="center" alignItems="center">
+      {t("footer")}
+    </Flex>
+  );
+};
