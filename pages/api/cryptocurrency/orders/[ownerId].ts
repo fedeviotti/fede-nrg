10b3406bd2af4add033bd7111649e97cc3e79414
Federico Viotti <fedeviotti@gmail.com>
2022-11-13 10:58:24 +0100

    chore(crypto): update prisma query

--- pages/api/cryptocurrency/orders/[ownerId].ts
+++ pages/api/cryptocurrency/orders/[ownerId].ts
@@ -3,7 +3,7 @@ import { prisma } from "~/lib/prisma";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const { ownerId }: { ownerId?: string } = req.query;
-  const orders = await prisma.young_orders.findMany({
+  const orders = await prisma.youngOrders.findMany({
     where: {
       owner_id: ownerId,
     },
